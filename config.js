3f1d40d6abe33dded3e98b1bc120587b6cdb7319
ZulfikarAkbar <zulfikar.78.akbar@gmail.com>
2020-12-12 10:15:16 +0700

    42th commit

--- config.js
+++ config.js
@@ -275,26 +275,26 @@ function message_order()
         liff.getProfile()
             .then(profile => {
                 msg += profile.displayName + ' ' + 'just bought something! '
+                var total_prices = 0;
+                for(i in order_data)
+                {
+                    total_prices = total_prices+order_data[i].prices;
+                    msg += '(' + order_data[i].qty + 'pcs ' + order_data[i].menu_name + ' with price total = $' + order_data[i].prices + ' ~ ' + '1pcs = $' + order_data[i].price +'), '
+                }
+                msg+=' with TOTAL ALL = $' + total_prices + '. Thanks!'
+                modal_msg = msg + '. Please see your LINE message for your order history.'
+                line_msg = '[Order history: ' + created_date() + ']' + ' ' + msg
+                modal_success_msg(modal_msg);
+                liff.sendMessages([{
+                    'type': 'text',
+                    'text': line_msg
+                }]).catch(function(error) {
+                    err_msg = 'Error sending message: ' + error;
+                    modal_err_msg(err_msg);
+                });
         })
         .catch((err) => {
             console.log('error', err);
-        });
-        var total_prices = 0;
-        for(i in order_data)
-        {
-            total_prices = total_prices+order_data[i].prices;
-            msg += '(' + order_data[i].qty + 'pcs ' + order_data[i].menu_name + ' with price total = $' + order_data[i].prices + ' ~ ' + '1pcs = $' + order_data[i].price +'), '
-        }
-        msg+=' with TOTAL ALL = $' + total_prices + '. Thanks!'
-        modal_msg = msg + '. Please see your LINE message for your order history.'
-        line_msg = '[Order history: ' + created_date() + ']' + ' ' + msg
-        modal_success_msg(modal_msg);
-        liff.sendMessages([{
-            'type': 'text',
-            'text': line_msg
-        }]).catch(function(error) {
-            err_msg = 'Error sending message: ' + error;
-            modal_err_msg(err_msg);
         });
     }
 }
