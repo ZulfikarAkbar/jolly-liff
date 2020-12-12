8f909197fcca1d8c7c6f415bc8c032a895861c60
ZulfikarAkbar <zulfikar.78.akbar@gmail.com>
2020-12-12 10:19:50 +0700

    43th commit

--- config.js
+++ config.js
@@ -282,19 +282,16 @@ function message_order()
                     msg += '(' + order_data[i].qty + 'pcs ' + order_data[i].menu_name + ' with price total = $' + order_data[i].prices + ' ~ ' + '1pcs = $' + order_data[i].price +'), '
                 }
                 msg+=' with TOTAL ALL = $' + total_prices + '. Thanks!'
-                modal_msg = msg + '. Please see your LINE message for your order history.'
-                line_msg = '[Order history: ' + created_date() + ']' + ' ' + msg
-                modal_success_msg(modal_msg);
-                liff.sendMessages([{
-                    'type': 'text',
-                    'text': line_msg
-                }]).catch(function(error) {
-                    err_msg = 'Error sending message: ' + error;
-                    modal_err_msg(err_msg);
-                });
-        })
-        .catch((err) => {
-            console.log('error', err);
+        });
+        modal_msg = msg + '. We also send this order history to your LINE message.'
+        line_msg = '[Order history: ' + created_date() + ']' + ' ' + msg
+        modal_success_msg(modal_msg);
+        liff.sendMessages([{
+            'type': 'text',
+            'text': line_msg
+        }]).catch(function(error) {
+            err_msg = 'Error sending message: ' + error;
+            modal_err_msg(err_msg);
         });
     }
 }
